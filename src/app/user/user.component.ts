7ff3c6de8bf44fc70a4cdf22703c59493c91bc37
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-02-22 17:18:03 +0100

    video_51

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,15 +8,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit{
   user: { id: string; name: string; } | any; 
-  
+    
   constructor(private route: ActivatedRoute) {    
   }
+  //ActivatedRoute : ActivatedRoute = la route actuelle
+  // ma3naha kayn un lien li rah mowafi9 l une component f routing w rah nab3thou bih des 
+  // parametre, dans notre cas rah nesta9blou hna deux parametres de type string
 
   ngOnInit(): void {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
+      //quand on utilise this activate route snapshat il ya un probleme
     }
+    this.route.params.subscribe((data: Params) => { //derna hakda bech juste yetbadlou id w name f lien nrecupiriwhom hna
+      this.user = {
+        id: data['id'],
+        name: data['name'],
+      }
+    });
   }
 
  
