0becff1cf3c8f961b51d75bcf07a5a9e0de3e56c
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-02-22 14:26:52 +0100

    video_49

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+  ngOnInit(): void {
+  }
+  id=10;
+  constructor(private router: Router) {    
+  }
 
+  onCategoriesClick(){
+    this.router.navigateByUrl('/categories');
+    // ou bien :  this.router.navigate(['/categories',id, etc]); avec cette méthode on peut passer qlq autres choses!
+  }
 }
