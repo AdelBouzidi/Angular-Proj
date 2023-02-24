13a344a4e4eff6ea15782100b97284be0e706ec7
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-02-24 17:10:32 +0100

    video_57

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CategoriesComponent } from "./categories/categories.component";
+import { EditUserComponent } from "./edit-user/edit-user.component";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { UserComponent } from "./user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const appRoutes: Routes = [
+    {path: '', component: HomeComponent}, //localhost;4200/
+   
+    {path: 'users', 
+    component: UsersComponent,
+    children: [{path: ':id/:name', component: UserComponent},
+    {path: ':id/:name/edit', component: EditUserComponent}],
+    },
+  
+    // {path: 'users/:id/:name', component:UserComponent},
+    {path: 'categories', component: CategoriesComponent},
+    
+    {path: 'user', component: UserComponent},
+    
+    // { path: '**', pathMatch: 'full',
+    // component: PageNotFoundComponent } ou bien
+    { path: 'not-found', component: PageNotFoundComponent},
+    { path: '**', redirectTo: 'not-found' }, //hadi toujours tkoun lekhra kamel f routage tdirha lfou9 yasra mochkil mafhmtouch
+  ];
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,32 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent}, //localhost;4200/
+// const appRoutes: Routes = [
+//   {path: '', component: HomeComponent}, //localhost;4200/
  
-  {path: 'users', 
-  component: UsersComponent,
-  children: [{path: ':id/:name', component: UserComponent},
-  {path: ':id/:name/edit', component: EditUserComponent}],
-  },
+//   {path: 'users', 
+//   component: UsersComponent,
+//   children: [{path: ':id/:name', component: UserComponent},
+//   {path: ':id/:name/edit', component: EditUserComponent}],
+//   },
 
-  // {path: 'users/:id/:name', component:UserComponent},
-  {path: 'categories', component: CategoriesComponent},
+//   // {path: 'users/:id/:name', component:UserComponent},
+//   {path: 'categories', component: CategoriesComponent},
   
-  {path: 'user', component: UserComponent},
+//   {path: 'user', component: UserComponent},
   
-  // { path: '**', pathMatch: 'full',
-  // component: PageNotFoundComponent } ou bien
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: 'not-found' }, //hadi toujours tkoun lekhra kamel f routage tdirha lfou9 yasra mochkil mafhmtouch
-];
-
+//   // { path: '**', pathMatch: 'full',
+//   // component: PageNotFoundComponent } ou bien
+//   { path: 'not-found', component: PageNotFoundComponent},
+//   { path: '**', redirectTo: 'not-found' }, //hadi toujours tkoun lekhra kamel f routage tdirha lfou9 yasra mochkil mafhmtouch
+// ];
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,9 +42,10 @@ const appRoutes: Routes = [
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(appRoutes)
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
