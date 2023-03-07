439273206d68506ec772999af05abd737c265f2e
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-03-07 21:35:07 +0100

    video_75

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,25 +8,26 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateFormComponent implements OnInit{
   @ViewChild('f') signUpForm: NgForm | any;
-
+  gender='male';
+  about='my name id adel';
   ngOnInit(): void {
   }
   constructor() {    
   }
 
-  checkData(){
-    console.log(this.signUpForm);
-  }
+  // checkData(){
+  //   console.log(this.signUpForm);
+
+  // }
   // onFormSubmit(f: NgForm){
-  //   console.log(f.value.username);
+  //   console.log(f.value.username)
   //   console.log(f.value.email);
   //   console.log(f.value.gender);
   // }  
   onFormSubmit(){
-    console.log(this.signUpForm.value.username);
-    console.log(this.signUpForm.value.email);
-    console.log(this.signUpForm.value.gender);
+    console.log(this.signUpForm);
+ 
   } 
-
+  
 
 }
