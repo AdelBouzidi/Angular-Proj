98d0367809742ad2861e384203e6d336f37aa1e0
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-03-08 19:44:57 +0100

    video-82

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -13,13 +13,29 @@ export class ReactiveFormsComponent implements OnInit{
 
   constructor() {    
   }
+  //without Grouping the Controls :
+  // ngOnInit(): void {
+  //   //we need to create an object of the form Group, and i need to give the controls present in the form Group (inpusts) :
+  //   this.signUpForm = new FormGroup({
+  //     'username': new FormControl(null, Validators.required), //username is a form control you need to remember this, null <=> default value.
+  //     //Validators is a static method 
+  //     'email': new FormControl(null, [Validators.required, Validators.email]), //form control 2
+  //     'gender': new FormControl('female') //form control 3
+  //   })
+  // }//after defining the validators we need to define the error message:
+
+  ////with Grouping the Controls :
   ngOnInit(): void {
     //we need to create an object of the form Group, and i need to give the controls present in the form Group (inpusts) :
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null, Validators.required), //username is a form control you need to remember this, null <=> default value.
-      //Validators is a static method
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('female')
+      // 'userData': new FormGroup({}), : is the first thing to do for Grouping the Controls in the Reactive Forms using FormGroupName in FormGroup
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required), //username is a form control you need to remember this, null <=> default value.
+        //Validators is a static method 
+        'email': new FormControl(null, [Validators.required, Validators.email]), //form control 2
+      }), 
+     
+      'gender': new FormControl('female') //form control 3
     })
   }//after defining the validators we need to define the error message:
 
