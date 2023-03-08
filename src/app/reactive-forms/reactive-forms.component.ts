b2b5cc5f5eea2dcff08fbe83d90fce550158e8e6
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-03-08 17:52:09 +0100

    video_81

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -16,11 +16,12 @@ export class ReactiveFormsComponent implements OnInit{
   ngOnInit(): void {
     //we need to create an object of the form Group, and i need to give the controls present in the form Group (inpusts) :
     this.signUpForm = new FormGroup({
-      'username': new FormControl(null), //username is a form control you need to remember this, null <=> default value.
-      'email': new FormControl(null),
+      'username': new FormControl(null, Validators.required), //username is a form control you need to remember this, null <=> default value.
+      //Validators is a static method
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('female')
     })
-  }
+  }//after defining the validators we need to define the error message:
 
   onSubmit(){
     console.log(this.signUpForm);
