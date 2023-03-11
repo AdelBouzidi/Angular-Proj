c284a574dbaa911f1b3ab565f8efc3349ff18079
adelbouzidi <adel.bouzidi.ci@gmail.com>
2023-03-11 20:55:35 +0100

    video_93

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http' //http
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
+import { ArrayType } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -9,7 +11,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class PostsComponent implements OnInit{
   postForm: FormGroup | any;
-
+  posts:any;
   constructor( private http: HttpClient) {
   }
   ngOnInit(): void {
@@ -17,13 +19,27 @@ export class PostsComponent implements OnInit{
       title: new FormControl(null, [Validators.required]),
       content: new FormControl(null, [Validators.required])
     });
+    this.getPost();
+  }
+
+  getPost(){
+    this.http.get('https://ng-complete-guide-f9175-default-rtdb.europe-west1.firebasedatabase.app/posts.json').pipe(map((response :any) => {
+      let posts=[];
+      for(let key in response){ // in not of because it is an object
+        posts.push({...response[key], key});
+      }
+      return posts;
+    })).subscribe(response => {
+    //  console.log(response);
+     this.posts=response;
+    });
   }
 
   onCreatePost(){
     // console.log(this.postForm.value);
     const postData= this.postForm.value;
     this.http.post('https://ng-complete-guide-f9175-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData).subscribe(response =>{
-      console.log(response);
+      this.getPost();
     });
   }
 
